a4f8d65b16d21d8919545ec5c616e7de66c1e148
adesinna <aladesaea@yahoo.com>
2024-06-29 18:14:20 -0400

    filter

--- 2-Terraform/7-Terraform for_each Meta-Argument_with _Functions_toset_tomap/5-data_source.tf
+++ 2-Terraform/7-Terraform for_each Meta-Argument_with _Functions_toset_tomap/5-data_source.tf
@@ -26,3 +26,19 @@ data "aws_availability_zones" "my_az" {
     values = ["opt-in-not-required"] # it would auto fill
   }
 }
+
+# Check if that respective Instance Type is supported in that Specific Region in list of availability Zones
+# Get the List of Availability Zones in a Particular region where that respective Instance Type is supported
+# Datasource-2
+data "aws_ec2_instance_type_offerings" "my_ins_type" {
+  for_each = toset(data.aws_availability_zones.my_az.names)
+  filter {
+    name   = "instance-type"
+    values = ["t3.micro"]
+  }
+  filter {
+    name   = "location"
+    values = [each.key]
+  }
+  location_type = "availability-zone"
+}
