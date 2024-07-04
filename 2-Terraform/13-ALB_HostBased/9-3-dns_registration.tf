36e74d2487cf73901ce21599993b20087ec5f771
adesinna <aladesaea@yahoo.com>
2024-07-04 12:12:21 -0400

    host based

--- 2-Terraform/13-ALB_HostBased/9-3-dns_registration.tf
+++ 2-Terraform/13-ALB_HostBased/9-3-dns_registration.tf
@@ -10,3 +10,29 @@ resource "aws_route53_record" "apps_dns" {
     evaluate_target_health = true
   }  
 }
+
+# DNS Registration 
+resource "aws_route53_record" "apps_dns_1" {
+  zone_id = data.aws_route53_zone.mydomain.zone_id
+  name    = var.app1_dns_name # for app1
+  type    = "A"
+  alias { # route it here, A => ALB
+   
+    name                   = module.application-lb.dns_name # 
+    zone_id                = module.application-lb.zone_id
+    evaluate_target_health = true
+  }  
+}
+
+# DNS Registration 
+resource "aws_route53_record" "apps_dns_2" {
+  zone_id = data.aws_route53_zone.mydomain.zone_id
+  name    = var.app2_dns_name   # for app2
+  type    = "A"
+  alias { # route it here, A => ALB
+   
+    name                   = module.application-lb.dns_name
+    zone_id                = module.application-lb.zone_id
+    evaluate_target_health = true
+  }  
+}
